c1fe24868132def3ce27f9401bea937235fe4fd1
charlesthpa <39455404+charlesthpa@users.noreply.github.com>
2021-08-03 17:53:18 +0200

    Code_final

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -1,3 +1,4 @@
+
 # from __future__ import print_function
 from time import gmtime, strftime 
 from MicroExpNet import *
@@ -24,18 +25,21 @@ yellow=4000
 green=20000
 blue=42000
 purple=50000
-sum_predictions=np.zeros((1,1,8))
-final_sum_predictions=np.zeros((1,1,8))
+no_face_count=0
+sum_predictions=np.zeros((1,1,8)) #avoid toggle between no_face and face_detetected
+final_sum_predictions=np.zeros((1,1,8)) #for average probability for every emotion 
 
 labels = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
-emotion_color=[8000,2000,50000,16000,0,38000,46000,54000]
-color_names=["yellow", "red", "purple", "green", "dark red", "light blue", "dark blue", "pink"]
+#emotion_color=[8000,2000,50000,16000,0,38000,46000,54000] 
+emotion_color=[0,2000,60000,16000,20000,10000,46000,38000]#mapping meotion to color
+color_names=["yellow", "red", "purple", "green", "dark red", "light blue", "dark blue", "pink"] #colornames for emotions
 modelDir = "Models/CK/"
-bridge_ip="192.168.178.79"
-bridge_username="ZdgukOoXbpuwxKhcMioCljyeyCMKXIeJC0LmEtKI"
+hue_group_name='pixelflux'
+bridge_ip="192.168.20.101"#ip of hue bridge
+bridge_username="ZdgukOoXbpuwxKhcMioCljyeyCMKXIeJC0LmEtKI" #username to get permission for hue lights
 #hue functions
-def turn_on_group(where):
-    groups = { 'pixelflux': 1}
+def turn_on_group(where): #enable lights
+    groups = { hue_group_name: 1} #name of hue group
     group_id = groups[where]
 
     payload = {"on":True}
@@ -44,8 +48,8 @@ def turn_on_group(where):
 
 # Beispiel:
 # turn_off_group('kitchen')
-def turn_off_group(where):
-    groups = { 'pixelflux': 1}
+def turn_off_group(where): #disable hue lights
+    groups = { hue_group_name: 1}
     group_id = groups[where]
 
     payload = {"on":False}
@@ -53,54 +57,24 @@ def turn_off_group(where):
     r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload), headers=headers)
 
 def switch_light_color(where,what):
-    groups = { 'pixelflux': 1}
+    groups = { hue_group_name: 1}
     group_id = groups[where]
-
-    payload = {"on":True,"sat":254, "bri":254,"hue":what}
+    if what==0: #neutral
+        payload = {"on":True,"sat":0, "bri":128,"hue":27000} #daturation, brightness and color
+    else:
+        payload = {"on":True,"sat":254, "bri":200,"hue":what} #daturation, brightness and color
     headers = {'content-type': 'application/json'}
     r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload), headers=headers)
+def switch_to_white(where): #changes color to white
+    groups = { hue_group_name: 1}
+    group_id = groups[where]
 
-# functions for face_detection and face-marking
-def get_resize_factor(max_length):
-    image = rawCapture.array
-    width,height,channel=image.shape
-    print(image.shape)
-    rawCapture.truncate(0)
-    if (width>height):
-        target_width=max_length
-        resize_factor=width/target_width
-    elif height>width:
-        target_height=max_length
-        resize_factor=height/target_height
-    else: 
-        target_width=max_length
-        resize_factor=width/target_width
-    print(resize_factor)
-    return resize_factor,width,height
-
-def rect_to_bb(rect):
-    # take a bounding predicted by dlib and convert it
-    # to the format (x, y, w, h) as we would normally do
-    # with OpenCV
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
-    # return a tuple of (x, y, w, h)
-    return (x, y, w, h)
-
-def shape_to_np(shape, dtype="int"):
-    # initialize the list of (x, y)-coordinates
-    coords = np.zeros((68, 2), dtype=dtype)
-    # loop over the 68 facial landmarks and convert them
-    # to a 2-tuple of (x, y)-coordinates
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-    # return the list of (x, y)-coordinates
-    return coords
+    payload = {"on":True,"sat":0, "bri":255,"hue":27000}
+    headers = {'content-type': 'application/json'}
+    r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload),headers=headers)
 
 # Import the xml files of frontal face and eye
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') #initalize cv2.face_detection
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml') 
 
 def detectFaces(img):
@@ -119,13 +93,13 @@ def segmentFace(inputFile, imgXdim, imgYdim):
 
     # Detect the face
     deteced_faces = detectFaces(inputFile)
-    # Return the whole image if it failed to detect the face
-    if len(deteced_faces) > 0:
-        (p,q,r,s) = deteced_faces[0]
+    # Return the whole image if it fails to detect the face
+    if len(deteced_faces) > 0: #at least one face detceted
+        (p,q,r,s) = deteced_faces[0] 
         img = inputFile[q:q+s, p:p+r]
         img = cv2.resize(img, (imgXdim, imgYdim)) 
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        return (img,p,q,r,s)
+        return (img,p,q,r,s) #return image and 
     else:
         return None
 
@@ -136,9 +110,9 @@ def segmentFace(inputFile, imgXdim, imgYdim):
 
 def get_time():
     return strftime("%a, %d %b %Y %X", gmtime())    
-# camera config 
-HOST = '192.168.178.24' # Enter IP or Hostname of your server
-PORT = 5555 # Pick an open Port (1000+ recommended), must match the server port
+# not used but can be used to stream over ethernet 
+#HOST = '192.168.178.24' # Enter IP or Hostname of your server
+#PORT = 5555 # Pick an open Port (1000+ recommended), must match the server port
 #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s.connect((HOST,PORT))
 # initialze picamera 
@@ -151,18 +125,10 @@ rawCapture.truncate(0)
 time.sleep(0.1)
 
     # Static parameters
-imgXdim = 84
+imgXdim = 84 #size of image for emotion recognition
 imgYdim = 84
 nInput = imgXdim*imgYdim # Since RGB is transformed to Grayscale
-"""
-image = segmentFace(rawCapture.array, imgXdim, imgYdim)
-image_nr=0
-fin_emotions=[0]*len(labels)
-final_emotion=0
-no_face=0
-testX = np.reshape(image, (1, imgXdim*imgYdim))
-testX = testX.astype(np.float32)
-"""
+
 x = tf.placeholder(tf.float32, shape=[None, nInput])
 
 
@@ -181,7 +147,7 @@ weights_biases_deployer = tf.train.Saver({"wc1": classifier.w["wc1"], \
 
 emotions = deque(maxlen=15)
 
-turn_on_group('pixelflux')
+turn_on_group(hue_group_name)
 with tf.Session() as sess:
     # Initializing the variables
     sess.run(tf.global_variables_initializer())
@@ -189,13 +155,13 @@ with tf.Session() as sess:
     weights_biases_deployer.restore(sess, tf.train.latest_checkpoint(modelDir))
     print("[" + get_time() + "] Weights & Biases are restored.")
     last_emotion = ""
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):#take a picture
         t1=time.time()
-        image = frame.array
+        image = frame.array #extract image
         #face recognition and segmentation
-        image_rec = segmentFace(image, imgXdim, imgYdim)
+        image_rec = segmentFace(image, imgXdim, imgYdim) #detects the face
 
-        #image=cv2.resize(image,(800,600))
+        #screen size 1024*600
         ww = 1024
         hh = 600
         color = (0,0,0)
@@ -203,9 +169,10 @@ with tf.Session() as sess:
         result = np.full((hh,ww,cc), color, dtype=np.uint8)
         x_offset=324
         y_offset=0;
-        result[y_offset:y_offset+image.shape[0], x_offset:x_offset+image.shape[1]] = image
+        #adding black border on the left side to leave space for text
+        result[y_offset:y_offset+image.shape[0], x_offset:x_offset+image.shape[1]] = image 
 
-        if image_rec is not None:
+        if image_rec is not None: #face detected
             image_seg,x1,y1,w1,h1=image_rec
             cv2.putText(result, "Face Detected", (50,50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
             cv2.rectangle(result,(x1+x_offset,y1),(x1+w1+x_offset,y1+h1),(0,255,0),1)
@@ -223,7 +190,7 @@ with tf.Session() as sess:
 
 
             # control hue lights        
-            if(len(emotions)==10):
+            if(len(emotions)==10): #vector captures the last emotions with highest percentage
                 final_sum_predictions=np.divide(sum_predictions,10)
                 counts = np.bincount(emotions)
                 final_emotion = np.argmax(counts)
@@ -232,11 +199,10 @@ with tf.Session() as sess:
 
                # cv2.putText(result, labels[emotion], (50,50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
                 #cv2.putText(result, labels[final_emotion], (50,75), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1)
-                sum_predictions=np.zeros((1,1,8))
+                sum_predictions=np.zeros((1,1,8)) 
                 emotions.clear()
 
-            #label image
-            #print("[" + get_time() + "] Emotion: " + labels[argmax])
+            #percentage of every emotion
            
             neutral_percentage=str((final_sum_predictions[0][0][0]*100).round(1,None))
             anger_percentage=str((final_sum_predictions[0][0][1]*100).round(1,None))
@@ -247,6 +213,7 @@ with tf.Session() as sess:
             sadness_percentage=str((final_sum_predictions[0][0][6]*100).round(1,None))
             surprise_percentage=str((final_sum_predictions[0][0][7]*100).round(1,None))
             
+            #print all emotins an probability
             cv2.putText(result, labels[1]+": "+anger_percentage+"%", (50,400),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
             cv2.putText(result, labels[3]+": "+disgust_percentage+"%", (50,425),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
             cv2.putText(result, labels[4]+": "+fear_percentage+"%", (50,450),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
@@ -259,28 +226,25 @@ with tf.Session() as sess:
             
             final_emotion=np.argmax(final_sum_predictions)
             cv2.putText(result, "You're emotion is:", (50,90),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
-            cv2.putText(result, labels[final_emotion], (50,140),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1)
+            cv2.putText(result, labels[final_emotion], (15,240),  cv2.FONT_HERSHEY_SIMPLEX,2, (0, 255, 0), 1)
             if labels[final_emotion] != last_emotion:
-                    switch_light_color('pixelflux',emotion_color[final_emotion])
-                    last_emotion = labels[final_emotion]
-
-            t2=time.time()
-            
+                    #if emotion_color[final_emotion]!=0:
+                switch_light_color(hue_group_name,emotion_color[final_emotion])
+                last_emotion = labels[final_emotion]
+                    #else:
+                #switch_to_white(hue_group_name)
 
         else:
-            cv2.putText(result, "No Face Detected", (50,50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            cv2.putText(result, "No Face Detected", (25,50),  cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            no_face_count+=1
+            if(no_face_count==15):
+                no_face_count=0
+                turn_off_group(hue_group_name)
         rawCapture.truncate(0)
-        #marks start
-        #start=(27119).to_bytes(3,'big')
-        #s.send(start)
-        #s.send(image)
-        
-        #cv2.copyMakeBorder(image,0,0,200,0,cv2.BORDER_CONSTANT,value=[255,0,0])
+       #print image fullscreen
         cv2.namedWindow("emotion_recognition", cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty("emotion_recognition",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
         cv2.imshow("emotion_recognition",result)
         cv2.waitKey(1)
-        #print("versenden:",time.time()-t1)
-        #print("aufnehmen und senden:",time.time()-t2)
-        #print("emotion"+labels[argmax])
+      
 
